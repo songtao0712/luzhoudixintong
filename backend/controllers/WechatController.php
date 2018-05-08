a95b40d809bfc07ff255da159a07c2159c448bc5
admin-PC\admin <478938840@qq.com>
2018-05-08 14:53:40 +0800

    重新来

---
+++ backend/controllers/WechatController.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2018/5/8
+ * Time: 14:49
+ */
+namespace backend\Controllers;
+
+use EasyWeChat\Factory;
+use yii\web\Controller;
+
+class WechatController extends Controller
+{
+
+    public function actionIndex()
+    {
+        $config = [
+            'app_id'=>'wxbbd06c6491fa437c',
+            'secret'=>'c86f519a83d31e1ded005799b5625872',
+            'response_type'=>'array',
+            'log'=>[
+                'level'=>'debug',
+                'file'=>__DIR__.'/wechat.log',
+            ]
+        ];
+        $app = Factory::officialAccount($config);
+        $response = $app->server->serve();
+        $response->send();
+    }
+}
