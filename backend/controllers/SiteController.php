b88c2cc426e728909ee27825d1f199ed47aa82fe
admin-PC\admin <478938840@qq.com>
2018-05-08 14:42:39 +0800

    测试

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -57,10 +57,7 @@ class SiteController extends Controller
     public function actionIndex()
     {
         $app = Factory::officialAccount($this->config);
-
-        var_dump($app);die;
         $response = $app->server->serve();
-        // 将响应输出
         $response->send();
     }
 
