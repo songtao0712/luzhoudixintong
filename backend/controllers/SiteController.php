e91f27182587d951177ba3ff6a36f4e7064a520b
admin-PC\admin <478938840@qq.com>
2018-05-08 11:19:36 +0800

    第一次推送，并且已经集成easywechat

---
+++ backend/controllers/SiteController.php
@@ -0,0 +1,64 @@
+<?php
+namespace backend\controllers;
+
+use EasyWeChat\Factory;
+use Yii;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use common\models\LoginForm;
+
+/**
+ * Site controller
+ */
+class SiteController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    /*public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'actions' => ['login', 'error'],
+                        'allow' => true,
+                    ],
+                    [
+                        'actions' => ['logout', 'index'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }*/
+
+    /**
+     * {@inheritdoc}
+     */
+    /*public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+        ];
+    }*/
+
+    public function actionEchostr()
+    {
+        $app = Factory::officialAccount($this->config);
+        $response = $app->server->sever();
+        $response->send();
+    }
+
+}
